ab8bad7f6af983d82f875fdd218e81b010d79986
Jin-Sih Lin <linpct@gmail.com>
2016-05-29 06:04:13 +0800

    init

---
+++ tns_init.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+  echo 'Please input <project-name>'
+  exit
+fi
+
+PROJECT_NAME=$1
+
+# new
+tns create $PROJECT_NAME --ng
+
+# use coffeescript and jade
+cd $PROJECT_NAME
+tns platform add ios
+tns install coffeescript
+tns install jade
+tns install sass
+
+cd -
+
+# done
+echo 'done'
---
+++ tns_update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+npm i -g nativescript coffee-script
+gem install cocoapods xcodeproj
+gem update cocoapods xcodeproj
