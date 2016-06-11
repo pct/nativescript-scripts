9bfd93e1126f3fba020539bd84ee53c00e539577
Jin-Sih Lin <linpct@gmail.com>
2016-06-12 00:07:39 +0800

    add app/shared/config.ts

--- app/pages/index/index.component.ts
+++ app/pages/index/index.component.ts
@@ -1,11 +1,17 @@
 import {Component} from "@angular/core";
 
+import Config from '../../shared/config';
+
 @Component({
   selector: "Index",
   templateUrl: './pages/index/index.xml',
 })
 
 export class IndexPage {
+  public get navTitle(): string {
+    return Config.title;
+  }
+
   public counter: number = 16;
 
   public get message(): string {
---
+++ app/shared/config.ts
@@ -0,0 +1,3 @@
+export default class Config {
+  static title: string = '4Point Design';
+}
